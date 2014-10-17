d267865e21d6de787ecfc61ef020e0ccff306c64
App Academy Student <student@appacademy.io>
2014-10-17 16:58:59 -0700

    Make terrible hand comparison logic

--- lib/hand.rb
+++ lib/hand.rb
@@ -6,9 +6,111 @@ class Hand
   
   def add_cards(spec_cards)
     raise "TOO MANY CARDS" if (cards.size + spec_cards.size) > 5
-    @cards += spec_cards
+    @cards.concat(spec_cards)
   end
   
   def discard(spec_cards)
+    if spec_cards.length > cards.length
+      raise "Can't discard more cards than are in your hand" 
+    end
+    @cards -= spec_cards
   end
+  
+  def hand_type
+    rank_freqs = check_rank_frequencies.values
+    suit_freqs = check_suits.values
+    if straight? && suit_freqs.max == 5
+      return :straight_flush
+    elsif rank_freqs.max == 4
+      return :four_of_a_kind
+    elsif rank_freqs.max == 3 && rank_freqs.min == 2
+      return :full_house
+    elsif suit_freqs.max == 5
+      return :flush
+    elsif straight?
+      return :straight
+    elsif rank_freqs.max == 3
+      return :three_of_a_kind
+    elsif rank_freqs.max == 2 && rank_freqs.length == 3
+      return :two_pair
+    elsif rank_freqs.max == 2
+      return :pair
+    else
+      return :high_card
+    end  
+  end
+  
+  def check_rank_frequencies
+    same_rank = Hash.new(0)
+    cards.each do |card|
+      same_rank[card.rank] += 1
+    end
+    same_rank 
+  end
+  
+  def straight?
+    values = card_values
+    return true if values == [0, 9, 10, 11, 12]
+    (values.min..(values.max)).to_a == values
+  end
+  
+  def check_suits
+    suits = Hash.new(0)
+    cards.each do |card|
+      suits[card.suit] += 1
+    end
+    suits
+  end
+  
+  def card_values
+    values = []
+    @cards.each do |card|
+      values << Card.ranks.index(card.rank)
+    end
+    values.sort
+  end
+  
+  def beats?(other_hand)
+    if hand_rank < other_hand.hand_rank
+      return true
+    elsif hand_rank > other_hand.hand_rank
+      return false
+    end
+    if [:straight_flush, :flush, :straight, :high_card].include?(hand_type)
+      checking_values = card_values
+      checking_values = [9, 10, 11, 12, 13] if checking_values == [0, 9, 10, 11, 12]
+      opp_values = other_hand.card_values
+      opp_values = [9, 10, 11, 12, 13] if opp_values == [0, 9, 10, 11, 12]
+      if checking_values.min < opp_values.min
+        return true
+      else
+        return false
+      end
+    elsif hand_type == :two_pair
+      check_rank_frequencies
+    else
+      hand_freqs = check_rank_frequencies
+      opp_freqs = other_hand.check_rank_frequencies
+      comp_card = hand_freqs.select { |rank, freq| freq == hand_freqs.values.max}
+      opp_card = opp_freqs.select { |rank, freq| freq == opp_freqs.values.max}
+      comp_rank = Card.ranks.index(comp_card.keys[0])
+      opp_rank = Card.ranks.index(opp_card.keys[0])
+      return comp_rank < opp_rank
+    end
+    false
+  end
+  
+  def hand_rank
+    HAND_ORDER.index(hand_type)
+  end
+  
+  HAND_ORDER = [:straight_flush, 
+                :four_of_a_kind,
+                :full_house,
+                :flush,
+                :straight,
+                :three_of_a_kind,
+                :two_pair,
+                :pair,
+                :high_card]
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,4 +1,5 @@
 require 'hand.rb'
+require 'debugger'
 
 describe "Hand" do
   subject(:hand) {Hand.new}
@@ -21,8 +22,327 @@ describe "Hand" do
         .to raise_error("TOO MANY CARDS")
     end
     it "should be able to get rid of cards" do
+      leftover_hand = hand.cards[1..4]
+      hand.discard([hand.cards[0]])
+      expect(hand.cards).to eq(leftover_hand)
     end
-    it "tells what hand it is"
-    it "tells what hands beat other hands"
+  end
+  
+  describe "when showing down" do
+    
+    describe "tells what hand it is" do
+        let(:king_high) do
+          hand = Hand.new
+          hand.add_cards([Card.new(:spades, :king), 
+                              Card.new(:hearts, :ten), 
+                              Card.new(:diamonds, :seven), 
+                              Card.new(:clubs, :four),
+                              Card.new(:diamonds, :three)])
+          hand
+         end
+        let(:pair_of_aces) do
+          hand = Hand.new
+          hand.add_cards([Card.new(:spades, :ace), 
+                              Card.new(:hearts, :ace), 
+                              Card.new(:diamonds, :seven), 
+                              Card.new(:clubs, :four),
+                              Card.new(:diamonds, :three)])
+          hand
+        end
+        let(:kings_over_sevens) do
+          hand = Hand.new
+          hand.add_cards([Card.new(:spades, :king), 
+                              Card.new(:hearts, :king), 
+                              Card.new(:diamonds, :seven), 
+                              Card.new(:clubs, :seven),
+                              Card.new(:diamonds, :two)])
+          hand
+        end
+        let(:three_threes) do
+          hand = Hand.new
+          hand.add_cards([Card.new(:spades, :three), 
+                              Card.new(:hearts, :five), 
+                              Card.new(:diamonds, :three), 
+                              Card.new(:clubs, :three),
+                              Card.new(:diamonds, :two)])
+          hand
+        end
+        let(:straight_to_7) do
+          hand = Hand.new
+          hand.add_cards([Card.new(:spades, :five), 
+                              Card.new(:hearts, :six), 
+                              Card.new(:diamonds, :seven), 
+                              Card.new(:clubs, :four),
+                              Card.new(:diamonds, :three)])
+          hand
+        end
+        let(:ten_high_flush) do
+          hand = Hand.new
+          hand.add_cards([Card.new(:spades, :five), 
+                              Card.new(:spades, :six), 
+                              Card.new(:spades, :ten), 
+                              Card.new(:spades, :four),
+                              Card.new(:spades, :three)])
+          hand
+        end
+        let(:threes_full_of_twos) do
+          hand = Hand.new
+          hand.add_cards([Card.new(:spades, :three), 
+                              Card.new(:hearts, :two), 
+                              Card.new(:diamonds, :three), 
+                              Card.new(:clubs, :three),
+                              Card.new(:diamonds, :two)])
+          hand
+        end
+        let(:four_fours) do 
+          hand = Hand.new
+          hand.add_cards([Card.new(:spades, :four), 
+                              Card.new(:hearts, :four), 
+                              Card.new(:diamonds, :four), 
+                              Card.new(:clubs, :four),
+                              Card.new(:diamonds, :three)])
+          hand
+        end
+        let(:straight_flush) do
+          hand = Hand.new
+          hand.add_cards([Card.new(:spades, :five), 
+                              Card.new(:spades, :six), 
+                              Card.new(:spades, :seven), 
+                              Card.new(:spades, :four),
+                              Card.new(:spades, :three)])
+          hand
+        end
+        let(:straight_to_five) do
+          hand = Hand.new
+          hand.add_cards([Card.new(:spades, :five), 
+                              Card.new(:hearts, :two), 
+                              Card.new(:diamonds, :ace), 
+                              Card.new(:clubs, :four),
+                              Card.new(:diamonds, :three)])
+          hand
+        end
+      it "knows it is a high card hand" do
+        expect(king_high.hand_type).to eq(:high_card)
+      end
+      it "knows when it is a pair" do
+        expect(pair_of_aces.hand_type).to eq(:pair)
+      end
+      
+      it "knows when it is a two pair" do
+        expect(kings_over_sevens.hand_type).to eq(:two_pair)
+      end
+      it "knows when it is a three of a kind" do
+        expect(three_threes.hand_type).to eq(:three_of_a_kind)
+      end
+      it "knows when it is a straight" do
+        expect(straight_to_7.hand_type).to eq(:straight)
+      end
+      it "knows when it is a flush" do
+        expect(ten_high_flush.hand_type).to eq(:flush)
+      end
+      it "knows when it is a full house" do
+        expect(threes_full_of_twos.hand_type).to eq(:full_house)
+      end
+      it "knows when it is a four of a kind" do
+        expect(four_fours.hand_type).to eq(:four_of_a_kind)
+      end
+      it "knows when it is a straight flush" do
+        expect(straight_flush.hand_type).to eq(:straight_flush)
+      end
+      it "knows that an ace can be low in a straight" do
+        expect(straight_to_five.hand_type).to eq(:straight)
+      end
+    end
+    describe "what hands beat other hands" do
+      let(:king_high) do
+        hand = Hand.new
+        hand.add_cards([Card.new(:spades, :king), 
+                            Card.new(:hearts, :ten), 
+                            Card.new(:diamonds, :seven), 
+                            Card.new(:clubs, :four),
+                            Card.new(:diamonds, :three)])
+        hand
+       end
+      let(:pair_of_aces) do
+        hand = Hand.new
+        hand.add_cards([Card.new(:spades, :ace), 
+                            Card.new(:hearts, :ace), 
+                            Card.new(:diamonds, :seven), 
+                            Card.new(:clubs, :four),
+                            Card.new(:diamonds, :three)])
+        hand
+      end
+      let(:kings_over_sevens) do
+        hand = Hand.new
+        hand.add_cards([Card.new(:spades, :king), 
+                            Card.new(:hearts, :king), 
+                            Card.new(:diamonds, :seven), 
+                            Card.new(:clubs, :seven),
+                            Card.new(:diamonds, :two)])
+        hand
+      end
+      let(:three_threes) do
+        hand = Hand.new
+        hand.add_cards([Card.new(:spades, :three), 
+                            Card.new(:hearts, :five), 
+                            Card.new(:diamonds, :three), 
+                            Card.new(:clubs, :three),
+                            Card.new(:diamonds, :two)])
+        hand
+      end
+      let(:straight_to_7) do
+        hand = Hand.new
+        hand.add_cards([Card.new(:spades, :five), 
+                            Card.new(:hearts, :six), 
+                            Card.new(:diamonds, :seven), 
+                            Card.new(:clubs, :four),
+                            Card.new(:diamonds, :three)])
+        hand
+      end
+      let(:ten_high_flush) do
+        hand = Hand.new
+        hand.add_cards([Card.new(:spades, :five), 
+                            Card.new(:spades, :six), 
+                            Card.new(:spades, :ten), 
+                            Card.new(:spades, :four),
+                            Card.new(:spades, :three)])
+        hand
+      end
+      let(:threes_full_of_twos) do
+        hand = Hand.new
+        hand.add_cards([Card.new(:spades, :three), 
+                            Card.new(:hearts, :two), 
+                            Card.new(:diamonds, :three), 
+                            Card.new(:clubs, :three),
+                            Card.new(:diamonds, :two)])
+        hand
+      end
+      let(:four_fours) do 
+        hand = Hand.new
+        hand.add_cards([Card.new(:spades, :four), 
+                            Card.new(:hearts, :four), 
+                            Card.new(:diamonds, :four), 
+                            Card.new(:clubs, :four),
+                            Card.new(:diamonds, :three)])
+        hand
+      end
+      let(:straight_flush) do
+        hand = Hand.new
+        hand.add_cards([Card.new(:spades, :five), 
+                            Card.new(:spades, :six), 
+                            Card.new(:spades, :seven), 
+                            Card.new(:spades, :four),
+                            Card.new(:spades, :three)])
+        hand
+      end
+      let(:straight_to_five) do
+        hand = Hand.new
+        hand.add_cards([Card.new(:spades, :five), 
+                            Card.new(:hearts, :two), 
+                            Card.new(:diamonds, :ace), 
+                            Card.new(:clubs, :four),
+                            Card.new(:diamonds, :three)])
+        hand
+      end
+      let(:straight_to_king) do
+        hand = Hand.new
+        hand.add_cards([Card.new(:spades, :king), 
+                            Card.new(:hearts, :queen), 
+                            Card.new(:diamonds, :jack), 
+                            Card.new(:clubs, :ten),
+                            Card.new(:diamonds, :nine)])
+        hand
+      end
+      let(:pair_of_eights) do
+        hand = Hand.new
+        hand.add_cards([Card.new(:spades, :eight), 
+                            Card.new(:hearts, :eight), 
+                            Card.new(:diamonds, :seven), 
+                            Card.new(:clubs, :four),
+                            Card.new(:diamonds, :three)])
+        hand
+      end
+      let(:queens_over_sixes) do
+        hand = Hand.new
+        hand.add_cards([Card.new(:spades, :queen), 
+                            Card.new(:hearts, :queen), 
+                            Card.new(:diamonds, :six), 
+                            Card.new(:clubs, :six),
+                            Card.new(:diamonds, :two)])
+        hand
+      end
+      let(:three_sevens) do
+        hand = Hand.new
+        hand.add_cards([Card.new(:spades, :seven), 
+                            Card.new(:hearts, :five), 
+                            Card.new(:diamonds, :seven), 
+                            Card.new(:clubs, :seven),
+                            Card.new(:diamonds, :two)])
+        hand
+      end
+      let(:sevens_full_of_sixes) do
+        hand = Hand.new
+        hand.add_cards([Card.new(:spades, :seven), 
+                            Card.new(:hearts, :six), 
+                            Card.new(:diamonds, :seven), 
+                            Card.new(:clubs, :seven),
+                            Card.new(:diamonds, :six)])
+        hand
+      end
+      let(:four_sevens) do 
+        hand = Hand.new
+        hand.add_cards([Card.new(:spades, :seven), 
+                            Card.new(:hearts, :seven), 
+                            Card.new(:diamonds, :seven), 
+                            Card.new(:clubs, :seven),
+                            Card.new(:diamonds, :three)])
+        hand
+      end
+      it "knows that a pair beats high card" do
+        expect(pair_of_aces.beats?(king_high)).to be_true
+      end
+      it "knows that a two pair beats a pair" do
+        expect(kings_over_sevens.beats?(pair_of_aces)).to be_true
+      end
+      it "knows that a three of a kind beats a two pair" do
+        expect(three_threes.beats?(kings_over_sevens)).to be_true
+      end
+      it "knows that a straight beats a three of a kind" do
+        expect(straight_to_five.beats?(three_threes)).to be_true
+      end
+      it "knows that a flush beats a straight" do
+        expect(ten_high_flush.beats?(straight_to_five)).to be_true
+      end
+      it "knows that a full house beats a flush" do
+        expect(threes_full_of_twos.beats?(ten_high_flush)).to be_true
+      end
+      it "knows that a four of a kind beats a full house" do
+        expect(four_fours.beats?(threes_full_of_twos)).to be_true
+      end
+      it "knows that a straight flush beats a four of a kind" do
+        expect(straight_flush.beats?(four_fours)).to be_true
+      end
+      it "knows that a larger straight beats a smaller straight" do
+        expect(straight_to_king.beats?(straight_to_7)).to be_true
+      end
+      it "correctly compares A-5 straights" do
+        expect(straight_to_five.beats?(straight_to_7)).to be_false
+      end
+      it "compares pairs" do
+        expect(pair_of_aces.beats?(pair_of_eights)).to be_true
+      end
+      it "compares full house" do
+        expect(threes_full_of_twos.beats?(sevens_full_of_sixes)).to be_false
+      end
+      it "compares threes of a kind" do
+        expect(three_threes.beats?(three_sevens)).to be_false
+      end
+      it "compares two pairs" do
+        expect(kings_over_sevens.beats?(queens_over_sixes)).to be_true
+      end
+      it "compares four of a kind" do
+         expect(four_fours.beats?(four_sevens)).to be_false
+      end
+    end
   end
 end
